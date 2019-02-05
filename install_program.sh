8a1640b463ab75075b2de5c38021685f29d5d3e9
Felix Lehoux <felix.lehoux@hotmail.com>
2019-02-04 19:31:29 -0500

    Initial commit

---
+++ install_program.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+#Install dependencies
+apt-get install gconf-service gconf-service-backend gconf2 gconf2-common libgconf-2-4 python-doc python-tk python2-doc python2.7-doc binfmt-support libappindicator1 libindicator7 libpython-stdlib libpython2-stdlib python python-minimal python2 python2-minimal python2.7 python2.7-minimal -y
+
+#Install Curl
+apt-get install curl -y
+
+#Install git
+apt-get install git -y
+
+#Install net-tools (ifconfig)
+apt-get install net-tools -y
+
+#Install VirtualBox
+apt-get install virtualbox -y
+
+#Install Binwalk
+apt-get install binwalk -y
+
+#Install Foremost
+apt-get install foremost -y
+
+#Install Exiftool
+apt-get install libimage-exiftool-perl -y
+
+#Install Pngcheck
+apt-get install pngcheck -y
+
+#Install GDB
+apt-get install gdb -y
+
+#Install Steghide
+apt-get install steghide -y
+
+#Install Sonic Visualiser
+apt install sonic-visualiser -y
+
+#Install Stegosuite
+apt-get install stegosuite -y
+
+#Install 7z
+sudo apt install p7zip-full -y
+
+#Install Htop
+sudo apt  install htop -y
+
+#Spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get install spotify-client
+
+#Bitwarden
+dpkg -i programs_installation/Bitwarden-1.12.0-amd64.deb
+
+#Install Slack
+dpkg -i programs_installation/slack-desktop-3.3.3-amd64.deb
+
+#Install Burpsuite Community
+bash programs_installation/burpsuite_community_linux_v1_7_36.sh
+
+#Install 010Editor
+bash programs_installation/010EditorLinux64Installer
+
+#Install VisualStudio Code
+dpkg -i programs_installation/code_1.28.2-1539735992_amd64.deb
+
+#Install Google Chrome
+dpkg -i programs_installation/google-chrome-stable_current_amd64.deb
+
+#Install WPS (Office)
+dpkg -i programs_installation/wps-office_10.1.0.6757_amd64.deb
+
+#Install Keybase
+dpkg -i programs_installation/keybase_amd64.deb
+
+#Set my I3 configuration
+sudo -u L3houx git clone https://github.com/L3houx/I3-Setup.git
+sudo -u L3houx cp -r I3-Setup/* ~/.config/
+i3 restart
+
+
