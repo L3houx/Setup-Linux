99e574c6034caf79266526318411e5dec26b15e7
L3houx <felix.lehoux@hotmail.com>
2021-09-23 00:21:48 -0400

    Config files (installation)

---
+++ config_endeavouros_sway.sh
@@ -0,0 +1,32 @@
+
+
+# ------------------------ WAYLAND ------------------------
+## WDisplays
+sudo pacman -S meson -y
+cd ~/Downloads/
+git clone https://github.com/artizirk/wdisplays.git
+cd wdisplays
+meson build
+ninja -C build
+sudo ninja -C build install
+
+
+# ------------------------ APPS ------------------------
+## Keyboard layout
+sway input keyboard xkb_layout ca:multix
+
+## Bitwarden
+sudo pacman -Sy bitwarden
+
+## Rofi
+sudo pacman -Sy rofi
+
+## Discord
+sudo pacman -Sy discord
+
+## VSCode
+sudo pacman -Sy code
+
+
+## ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
